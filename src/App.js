ccdd7f4bb97a02c0096ea9c5f13e474eba6fd1a8
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-01-11 11:04:33 +0530

    u

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function Rings() {
             color="#a19266"
             metalness={1}
             roughness={1}
-            aoMap={a}
+            aoMap={ao}
             normalMap={normal}
             normalScale={[4, 4]}
             displacementMap={height}
@@ -114,5 +114,47 @@ function Rings() {
         </Torus>
       </group>
     </group>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <Canvas pixelRatio={[1, 1.5]} camera={{ position: [0, 1, 15], fov: 20 }}>
+        <fog attach="fog" args={["#e5e5e5", 10, 50]} />
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.1} />
+          <spotLight
+            castShadow
+            position={[0, 20, 20]}
+            intensity={6}
+            penumbra={1}
+            anglge={Math.PI / 3}
+            decay={2}
+            distance={40}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-focus={0.4}
+          />
+          <rectAreaLight
+            intensity={0.5}
+            args={["turquoise", 8, 8, 8]}
+            position={[0, -0.99, 0]}
+            rotation-x={Math.PI / 2}
+          />
+          <group position-y={-1}>
+            <Floor />
+            <Rings />
+          </group>
+          <Environment preset="dawn" />
+        </Suspense>
+        <OrbitControls
+          enablePan={false}
+          minPolarAngle={Math.PI / 16}
+          maxPolarAngle={Math.PI / 2}
+        />
+      </Canvas>
+      <Loader />
+    </>
   );
 }
